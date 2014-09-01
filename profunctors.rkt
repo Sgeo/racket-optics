a56e48ee3db9227c94ef6017abf4d94c50d7d68d
Sgeo <sgeoster@gmail.com>
2014-08-31 23:40:24 -0400

    Forget, provide, etc.
    provide its forms (mostly for internal use)
    Add forget profunctor, and a test for func.

--- profunctors.rkt
+++ profunctors.rkt
@@ -3,11 +3,27 @@
 (require racket/generic
          racket/function)
 
+(provide (struct-out func)
+         (struct-out forget)
+         gen:profunctor)
+
+(module+ test
+  (require rackunit))
+
 (define-generics profunctor
-  (dimap profunctor neg pos))
+  (dimap neg profunctor pos))
 
 (struct func (value)
   #:methods gen:profunctor
-  [(define (dimap profunctor neg pos)
+  [(define (dimap neg profunctor pos)
      (func (compose1 pos (func-value profunctor) neg)))])
 
+(module+ test
+  (check-eq?
+   ((func-value (dimap (curry * -1) (func add1) (curry * 10))) 5)
+   -40))
+
+(struct forget (value)
+  #:methods gen:profunctor
+  [(define (dimap neg profunctor _)
+     (compose1 (forget-value profunctor) neg))])
