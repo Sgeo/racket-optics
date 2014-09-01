c54d5c0f80767050769ab40b947a9efc7c5e689d
Sgeo <sgeoster@gmail.com>
2014-09-01 01:04:55 -0400

    Strength
    Add strength.

--- profunctors.rkt
+++ profunctors.rkt
@@ -16,17 +16,32 @@
 (define ((dimap-curried neg pos) profunctor)
   (dimap neg profunctor pos))
 
+(define-generics strong
+  (strong-first strong)) ; strong-first :: p a b -> p (a, o) (b, o)
+
 (struct func (value)
   #:methods gen:profunctor
   [(define (dimap neg profunctor pos)
-     (func (compose1 pos (func-value profunctor) neg)))])
+     (func (compose1 pos (func-value profunctor) neg)))]
+  #:methods gen:strong
+  [(define (strong-first strong)
+     (func (lambda (consed)
+             (cons
+              ((func-value strong) (car consed))
+              (cdr consed)))))])
 
 (module+ test
   (check-eq?
    ((func-value (dimap (curry * -1) (func add1) (curry * 10))) 5)
-   -40))
+   -40)
+  (check-equal?
+   ((func-value (strong-first (func add1))) (cons 5 "a"))
+   (cons 6 "a")))
 
 (struct forget (value)
   #:methods gen:profunctor
   [(define (dimap neg profunctor _)
-     (forget (compose1 (forget-value profunctor) neg)))])
+     (forget (compose1 (forget-value profunctor) neg)))]
+  #:methods gen:strong
+  [(define (strong-first strong)
+     )])
